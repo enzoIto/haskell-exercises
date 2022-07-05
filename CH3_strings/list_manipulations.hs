b772979882e6fe974d46b075df41d7833b086b7e
Enzo Ito <enzo.ito@madeiramadeira.com.br>
2022-07-05 10:37:15 -0300

    fix: substitute constant for var in functions

--- CH3_strings/list_manipulations.hs
+++ CH3_strings/list_manipulations.hs
@@ -1,10 +1,16 @@
 -- Input: "Hello World" -> "ello World"
-module StringsManipulation where
-    takeFirst = drop 1 x
-        where x = "Hello World"
+module StringsManipulation where 
+    takeFirst :: String -> String
+    takeFirst x = drop 1 x
+
     
 -- Input: "Curry is awesome" -> "Curry is awesome!"
-    putLast = (++) x "!"
-        where x = "Curry is awesome"
+    putLast :: String -> String
+    putLast x = (++) x "!"
     
 -- Input: "Curry is awesome!" -> "y"
+    takeNth :: String -> String
+    takeNth x = x !! 4
+
+    
+    
